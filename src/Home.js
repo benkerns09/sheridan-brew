92b46a2a892357d9ad9a682de5f78920930d308f
Ben Kerns <kernsb6@gmail.com>
2018-08-28 00:14:03 -0400

    Skeleton FrontEnd

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import "./Home.css";
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="homeBody">
                 <h2>"When you want it, how you want it"</h2>
-                    <p>This should have home page content. I'm not sure exactly what will go here yet.</p>
-
-                    <p>Maybe the imbedded youTube or an about us section about how the company started</p>
+                <p className="intro">
+                Sheridan Brew is an easy to use app based coffee delivery service that ofers high quality coffee products to your door. Retreiving your daily coffee does not need to be a 20 minute excursion. With the push of a button, get on with your day with Sheridan Brew.
+                </p>
+                <h3>Contact Us</h3>
+                <p className="phone">Office: 319-440-0699</p>
+                <p className="email">Email: coffee@sheridan.com</p>
             </div>
+
         );
     }
 }
